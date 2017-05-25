3b3661d4a28997e28fd5fc9b55f9dcdca3258887
Dan Zygmund-Felt <d.zygmund.felt@gmail.com>
2017-05-25 17:10:22 -0400

    Created project and .gitignore.

---
+++ src/main/java/zygmundfelt/dan/bitsetmicrolab/App.java
@@ -0,0 +1,13 @@
+package zygmundfelt.dan.bitsetmicrolab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
